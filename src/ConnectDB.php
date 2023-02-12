0dd6f6bce908bc76f6c597153541d284328b0e30
symphograph <58216294+symphograph@users.noreply.github.com>
2023-02-12 20:00:54 +1100

    add ConnectDB

--- src/ConnectDB.php
+++ src/ConnectDB.php
@@ -13,9 +13,12 @@ class ConnectDB
     {
     }
 
-    public static function byName(string $connectName): self
+    public static function byName(?string $connectName): self
     {
         $env = require dirname($_SERVER['DOCUMENT_ROOT']) . '/includes/env.php';
+        if(empty($connectName)){
+            $connectName = array_key_first($env->connects);
+        }
         $con = $env->connects->$connectName;
         return new self(
             host: $con->host, name: $con->name, user: $con->user, pass: $con->pass
