1352d0e81647a90f1e87c0cb4826f85910aee5a6
symphograph <58216294+symphograph@users.noreply.github.com>
2023-02-24 05:40:14 +1100

    fix err

--- src/Errors/ValidationErr.php
+++ src/Errors/ValidationErr.php
@@ -7,7 +7,7 @@ class ValidationErr extends MyErrors
     protected string $type = 'ValidationErr';
     protected bool $loggable = true;
 
-    public function __construct(string $message, string $pubMsg = '', int $httpStatus = 400)
+    public function __construct(string $message, string $pubMsg = 'Ошибка данных', int $httpStatus = 400)
     {
         parent::__construct($message, $pubMsg, $httpStatus);
     }
