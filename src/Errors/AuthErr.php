bdede1a5207e1322ad6c506f9a96a3f30a75262c
symphograph <58216294+symphograph@users.noreply.github.com>
2023-02-24 18:54:12 +1100

    add Logs

--- src/Errors/AuthErr.php
+++ src/Errors/AuthErr.php
@@ -5,7 +5,7 @@ namespace Symphograph\Bicycle\Errors;
 class AuthErr extends MyErrors
 {
     protected string $type = 'AuthErr';
-    public function __construct(string $message, string $pubMsg = 'Ошибка данных', int $httpStatus = 500)
+    public function __construct(string $message, string $pubMsg = 'Ошибка авторизации', int $httpStatus = 500)
     {
         parent::__construct($message, $pubMsg, $httpStatus);
     }
