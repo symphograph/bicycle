37f93205110b623c253c6a178254f0e1e75c3ba7
symphograph <58216294+symphograph@users.noreply.github.com>
2023-09-28 17:52:13 +1100

    refactor

--- src/DTO/BindTrait.php
+++ src/DTO/BindTrait.php
@@ -4,20 +4,27 @@ namespace Symphograph\Bicycle\DTO;
 
 trait BindTrait
 {
-    public function bindSelf(object|array $Object): void
+    public function bindSelf(object|array $object): void
     {
-        $Object = (object) $Object;
+        $object = (object) $object;
         $vars = get_class_vars($this::class);
         foreach ($vars as $k => $v) {
-            if (!isset($Object->$k)) continue;
-            $this->$k = $Object->$k;
+            if (!isset($object->$k)) continue;
+            $this->$k = $object->$k;
         }
     }
 
-    public static function byArr(object|array $Object): self
+    public static function byArr(object|array $object): self
     {
         $self = new self();
-        $self->bindSelf($Object);
+        $self->bindSelf($object);
+        return $self;
+    }
+
+    public static function byBind(object|array $data): self
+    {
+        $self = new self();
+        $self->bindSelf($data);
         return $self;
     }
 }
